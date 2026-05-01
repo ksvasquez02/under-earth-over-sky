cb225d695f010a4f03f6d228507d023439479686
ShovelCrow <ksvasquez02@gmail.com>
2026-05-01 13:33:09 -0400

    pre-build fixes

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+using TMPro;
 
 public class HUDManager : MonoBehaviour
 {
